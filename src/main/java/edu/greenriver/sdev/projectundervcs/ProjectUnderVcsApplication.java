8d2cb7f6a12875a7aa75b3bda1cda44f3878c7b8
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-01-09 14:59:02 -0800

    Added initial files for the project.

--- src/main/java/edu/greenriver/sdev/projectundervcs/ProjectUnderVcsApplication.java
+++ src/main/java/edu/greenriver/sdev/projectundervcs/ProjectUnderVcsApplication.java
@@ -1,5 +1,6 @@
 package edu.greenriver.sdev.projectundervcs;
 
+import edu.greenriver.sdev.projectundervcs.repositories.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,16 @@ public class ProjectUnderVcsApplication
     public static void main(String[] args)
     {
         SpringApplication.run(ProjectUnderVcsApplication.class, args);
+
+        Book book = new Book("THe Sword of Destiny", "Andrezej Sapkowski", 384);
+        Book defaultBook = new Book();
+
+        Book builtBook  = Book.builder()
+                .author("Randal Garrett")
+                .title("Lord d'Arcy Investigates")
+                .pages(229)
+                .build();
+
     }
 
 }
