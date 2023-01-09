106032fa7b5d1d0882fe210652b1e40579a7af51
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-01-09 13:31:08 -0800

    Added initial files for the project.

---
+++ src/main/java/edu/greenriver/sdev/projectundervcs/ProjectUnderVcsApplication.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.projectundervcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUnderVcsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProjectUnderVcsApplication.class, args);
+    }
+
+}
